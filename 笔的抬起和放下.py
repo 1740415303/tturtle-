07f889f597ce9820e79bc9ef349f691a2aeacf0e
1740415303 <74895352+1740415303@users.noreply.github.com>
2020-11-26 18:53:19 +0800

    Add files via upload

---
+++ 笔的抬起和放下.py
@@ -0,0 +1,17 @@
+import turtle as t
+
+t.circle(80)
+# t.pencolor('white')#更改笔的颜色
+# t.setx(-100 )#更改坐标
+# t.pencolor('green')
+# t.fd(100)
+
+#方法2
+#up() 抬起笔
+t.up()
+t.sety(-100)
+#down() 放下笔
+t.down()
+t.fd(150)
+t.mainloop()
+
